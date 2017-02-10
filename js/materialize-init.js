3d6bf2010a8cf14da144e9505c987524769d34a5
mariopolo2805 <mariopolo2805@gmail.com>
2017-02-10 19:04:38 +0100

    First structure

---
+++ js/materialize-init.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $(".button-collapse").sideNav();
+    $('.slider').slider();
+    $('.modal-trigger').leanModal();
+    $('select').material_select();
+    $('.parallax').parallax();
+});
