a95035a8d759b45a650a585b4626ce31c2d2ee6d
Mario Antonio Polo Daza <mariopolo2805@gmail.com>
2017-07-31 15:09:59 +0200

    Removed carousel

--- _site/assets/materialize-init.js
+++ _site/assets/materialize-init.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$(".slider").slider(),$(".parallax").parallax(),$(".carousel").carousel({dist:-10,shift:30,padding:20}),$("select").material_select(),$(".button-collapse").sideNav(),$(".modal-trigger").leanModal()});
+$(document).ready(function(){$(".slider").slider(),$(".parallax").parallax(),$("select").material_select(),$(".button-collapse").sideNav(),$(".modal-trigger").leanModal(),$(".materialboxed").materialbox()});
--- js/materialize-init.js
+++ js/materialize-init.js
@@ -1,12 +1,8 @@
 $(document).ready(function() {
     $('.slider').slider();
     $('.parallax').parallax();
-    $('.carousel').carousel({
-        dist: -10,
-        shift: 30,
-        padding: 20
-    });
     $('select').material_select();
-    $(".button-collapse").sideNav();
+    $('.button-collapse').sideNav();
     $('.modal-trigger').leanModal();
+    $('.materialboxed').materialbox();
 });
