184a45e529b1793887e89c227052c1cee88cc786
Mario Antonio Polo Daza <mariopolo2805@gmail.com>
2017-08-04 12:08:14 +0200

    Auto deploy

--- js/materialize-init.js
+++ js/materialize-init.js
@@ -4,5 +4,19 @@ $(document).ready(function() {
     $('select').material_select();
     $('.button-collapse').sideNav();
     $('.modal-trigger').leanModal();
-    $('.materialboxed').materialbox();
 });
+
+// Custom modal to fix .materialboxed problems
+var modal = document.getElementsByClassName('mamacarlota-modal')[0];
+var modalImg = document.getElementsByClassName('mamacarlota-modal-content')[0];
+var captionText = document.getElementsByClassName('mamacarlota-modal-caption')[0];
+
+function openModal(img) {
+    modal.style.display = 'block';
+    modalImg.src = img.src;
+    captionText.innerHTML = img.alt;
+}
+
+function closeModal() {
+    modal.style.display = 'none';
+}
