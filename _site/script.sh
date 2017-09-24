733f03e066e86734b422601241b9fe766b405eb6
Mario Antonio Polo Daza <mariopolo2805@gmail.com>
2017-09-24 23:35:37 +0200

    Auto deploy

--- _site/script.sh
+++ _site/script.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-find_command=$(find . -maxdepth 3 -name "*(1)*" -o -name "*(copia)*" -print)
-
 echo "Comprobando duplicados..."
 
 
-if [[ -n $find_command ]]
+if [[ -n $(find . -maxdepth 3 -name "*(1)*" -print) ]]
 then
-    find . -maxdepth 3 -name "*(1)*" -o -name "*(copia)*" -print
+    find . -maxdepth 3 -name "*(1)*" -print
+    exit 1
+fi
+
+if [[ -n $(find . -maxdepth 3 -name "*(copia)*" -print) ]]
+then
+    find . -maxdepth 3 -name "*(copia)*" -print
     exit 1
 fi
 
