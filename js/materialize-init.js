897402e613b26dbbff055678f4f7e4081c5891c9
Mario Antonio Polo Daza <mariopolo2805@gmail.com>
2017-10-05 19:58:13 +0200

    Auto deploy

--- js/materialize-init.js
+++ js/materialize-init.js
@@ -3,7 +3,11 @@ $(document).ready(function() {
     $('.parallax').parallax();
     $('select').material_select();
     $('.button-collapse').sideNav();
-    $('.modal-trigger').leanModal();
+    $('.modal').modal({
+      inDuration: 300, // Transition in duration
+      outDuration: 200, // Transition out duration
+    }
+  );
 });
 
 // Custom modal to fix .materialboxed problems
