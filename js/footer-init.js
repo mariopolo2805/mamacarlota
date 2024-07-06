e4e3fc31625b88fa12a27d36b2bfc2d331ff4aed
Mario Antonio Polo Daza <mariopolo2805@gmail.com>
2024-07-06 20:10:52 +0200

    fixes

--- js/footer-init.js
+++ js/footer-init.js
@@ -2,13 +2,11 @@ var footer = function () {
     // Get the terms markdown, compile it to HTML and append it to the page
     function loadTerms(path) {
         var converter = new showdown.Converter();
-        var result = null;
         $.ajax({
             url: path,
             type: 'get',
             dataType: 'html',
             success: function (data) {
-                debugger
                 $('#include-terms').append(converter.makeHtml(data));
             }
         });
